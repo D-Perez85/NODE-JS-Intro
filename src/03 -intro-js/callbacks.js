61721af920a34a7615a2da6b3107364bdc22ee3e
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-05-14 11:18:40 -0300

    Basic Callback

---
+++ src/03 -intro-js/callbacks.js
@@ -0,0 +1,15 @@
+//BASIC CALLBACK
+const getUserByID = (id, callback) => {
+    const user = {
+        id,
+        name: 'Damian'
+    }
+    setTimeout(() => {
+        console.log('Succes Callback')
+        callback(user)
+    }, 1500)
+}
+getUserByID(1, (user) => {
+    console.log(`Legajo nro: ${user.id}`);
+    console.log(`Name: ${user.name.toUpperCase()}`);
+});
