6941313995dd79bcec9504442f6a02581eb66f95
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-05-13 23:57:54 -0300

    Template Strings

---
+++ src/03 -intro-js/template-string.js
@@ -0,0 +1,27 @@
+
+const name = 'Deadpool';
+const real = 'Wade Winston';
+const age = 26; 
+
+const without = name + ' ' + real + ' ' + '- his age is ' + age;
+const include = `${ name } ${ real } - his age is ${age}`;
+
+console.log(without);
+console.log(include);
+
+const html = 
+`<h1>Hellow</h1>
+    <div>
+        <p>World</p>
+        <ul>
+            <li> Option A </li>
+            <li> Option B </li>
+        </ul>
+    </div>`;
+console.log(html);
+
+
+
+
+
+
