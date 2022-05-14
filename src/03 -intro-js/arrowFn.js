6bee9d5b14f4de347aba0c8677d71294f34ff3cd
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-05-14 11:10:47 -0300

    Arrow Function

---
+++ src/03 -intro-js/arrowFn.js
@@ -0,0 +1,17 @@
+
+//- Original Function
+// function sumar( a, b ){
+//     return a + b;
+// } 
+
+
+//- Arrow Function
+const sumar = (a, b) => a + b;
+const saludar = () => 'Hola Mundo';
+
+//EXECUTE FUNCTIONS
+console.log(sumar( 5, 10 ));
+console.log(saludar());
+
+
+
